9eb9419254f8d2f4d692b41ec513c3cd83c2f022
Nathan Jones <nathanmjones1@gmail.com>
2019-11-07 20:33:20 +0000

    Added rxjs devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreState } from './types/store.types';
 import { entityListReducers } from './store/reducers/entity-list.reducer';
 import { entitiesReducers } from './store/reducers/entity.reducer';
 import { entityRequestReducers } from './store/reducers/enity-request.reducer';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,6 +20,10 @@ import { entityRequestReducers } from './store/reducers/enity-request.reducer';
       entityLists: entityListReducers,
       entites: entitiesReducers,
       entityRequests: entityRequestReducers,
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
     }),
   ],
   providers: [],
