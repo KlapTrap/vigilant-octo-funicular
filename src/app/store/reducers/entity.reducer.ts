d2bd795cd91e035bb186c5d6d2320d85335ea90f
Nathan Jones <nathanmjones1@gmail.com>
2019-11-08 09:27:29 +0000

    Entity reducer now stores entity data

--- src/app/store/reducers/entity.reducer.ts
+++ src/app/store/reducers/entity.reducer.ts
@@ -1,8 +1,18 @@
 import { createReducer, on, Action } from '@ngrx/store';
 import { getBaseInitialState } from './reducers.helpers';
 import { Entities } from '../../types/store.types';
+import { fetchEntityListSuccess } from '../actions/entity-list.actions';
 
-const pEntitiesReducers = createReducer<Entities>(getBaseInitialState());
+const pEntitiesReducers = createReducer<Entities>(
+  getBaseInitialState(),
+  on(fetchEntityListSuccess, (state, action) => ({
+    ...state,
+    [action.entityType]: {
+      ...(state[action.entityType] || {}),
+      ...action.normalisedResponse.entities,
+    },
+  })),
+);
 
 export function entitiesReducers(state: Entities, action: Action) {
   return pEntitiesReducers(state, action);
