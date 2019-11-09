639b8d31a7f1a0bcf8c437d64b4dbe9b71f92328
Nathan Jones <nathanmjones1@gmail.com>
2019-11-09 23:46:34 +0000

    Removed unused file

--- src/store/reducers/entity-list.reducer.ts
+++
@@ -1,9 +0,0 @@
-import { createReducer, on, Action } from '@ngrx/store';
-import { EntityLists } from 'src/app/types/store.types';
-import { getBaseInitialState } from 'src/app/store/reducers/reducers.helpers';
-
-const pEntityListReducers = createReducer<EntityLists>(getBaseInitialState());
-
-export function entitiesReducers(state: EntityLists, action: Action) {
-  return pEntityListReducers(state, action);
-}
